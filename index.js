4816f7d88cddfc0fc438205033e64ece01015b71
vibhu mahendru <vibhumahendru@gmail.com>
2018-08-22 19:14:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,7 @@ function happyHolidaysTo(name){
 function happyHolidayTo(holiday, name){
   return(`Happy ${holiday}, ${name}!`)
 }
+
+function holidayCountdown (holiday, number){
+  return(`It's ${days} days until ${holiday}!`)
+}
